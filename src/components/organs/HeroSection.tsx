12064da522847f3a3012ce12cea9fcaed80fceee
sananayab <53163447+sananayab@users.noreply.github.com>
2023-12-09 00:44:53 +0500

    set code

--- src/components/organs/HeroSection.tsx
+++ src/components/organs/HeroSection.tsx
@@ -11,7 +11,7 @@ import { Fade, Slide } from "react-awesome-reveal"
 const HeroSection = () => {
     return (
         <section className="w-full lg:h-screen md:h-[550px] h-[830px] relative overflow-x-hidden flex justify-end">
-            <Image className="h-[90%] w-[70%] lg:h-[90vh] md:h-[50vh] lg:w-1/2 md:w-[55%]" image={bgImage} alt="Hero Background Vector" />
+            <Image className="h-[90%] w-[70%] lg:h-[90vh] md:h-[30vh] lg:w-1/2 md:w-[50%]" image={bgImage} alt="Hero Background Vector" />
             <main className="w-full lg:h-full h-auto grid md:grid-cols-2 absolute top-12 left-0 lg:px-24 md:px-8 px-5 pt-24 md:pt-32 lg:pt-0">
                 <div className="flex flex-col justify-center md:gap-6 gap-3 md:order-1 order-2">
                     <Text as="p" className="text-color1 uppercase tracking-widest lg:text-base  text-sm font-normal">
